a2dbb0dcc71827875cbbce29a072e03164f774db
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2022-12-08 21:40:13 +0300

    Added the workspace object class.

---
+++ Sources/IndustrialKit/WorkspaceObjects/WorkspaceObject.swift
@@ -0,0 +1,154 @@
+//
+//  WorkspaceObject.swift
+//  Robotic Complex Workspace
+//
+//  Created by Malkarov Park on 29.10.2022.
+//
+
+import Foundation
+import SceneKit
+import SwiftUI
+
+/**
+ A base class of industrial production object.
+ 
+ Industrial production objects are represented by equipment that provide technological operations performing.
+ */
+class WorkspaceObject: Identifiable, Equatable, Hashable, ObservableObject
+{
+    static func == (lhs: WorkspaceObject, rhs: WorkspaceObject) -> Bool //Identity condition by names
+    {
+        return lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher)
+    {
+        hasher.combine(name)
+    }
+    
+    public var id = UUID() //Object identifier
+    
+    ///Object name in workspace
+    public var name: String?
+    
+    init()
+    {
+        self.name = "None"
+    }
+    
+    init(name: String) //Init object by name. Used for mismatch.
+    {
+        self.name = name
+    }
+    
+    //MARK: - Object in workspace handling
+    ///In workspace placement state.
+    public var is_placed = false
+    
+    ///Object location components – *x*, *y*, *z*.
+    public var location = [Float](repeating: 0, count: 3)
+    
+    ///Object rotation components – *r*, *p*, *w*.
+    public var rotation = [Float](repeating: 0, count: 3)
+    
+    //MARK: - Visual functions
+    ///Scene file address.
+    public var scene_address = ""
+    
+    ///Connected object scene node.
+    public var node: SCNNode?
+    
+    ///Name of node for connect to instance node variable.
+    open var scene_node_name: String? { nil }
+    
+    ///Folder access bookmark.
+    public static var folder_bookmark: Data?
+    
+    ///Gets model node in scene imported from file.
+    public func get_node_from_scene()
+    {
+        do
+        {
+            //File access
+            var is_stale = false
+            let url = try URL(resolvingBookmarkData: WorkspaceObject.folder_bookmark ?? Data(), bookmarkDataIsStale: &is_stale)
+            
+            guard !is_stale else
+            {
+                return
+            }
+            
+            if scene_address != "" //If scene address not empty, get node from it
+            {
+                do
+                {
+                    self.node = try SCNScene(url: URL(string: url.absoluteString + scene_address)!).rootNode.childNode(withName: scene_node_name ?? "", recursively: false)
+                }
+                catch
+                {
+                    print(error.localizedDescription)
+                    node_by_description() //If node could not imported, create node model by description
+                }
+            }
+            else
+            {
+                node_by_description()
+            }
+        }
+        catch
+        {
+            print(error.localizedDescription)
+            node_by_description()
+        }
+    }
+    
+    ///Builds model by description without external scene.
+    open func node_by_description()
+    {
+        
+    }
+    
+    //MARK: - UI functions
+    ///Universal data storage for NSImage or UIImage.
+    public var image_data = Data()
+    
+    #if os(macOS)
+    ///Workspace object preview image.
+    public var image: NSImage
+    {
+        get
+        {
+            return NSImage(data: image_data) ?? NSImage() //Retrun NSImage from image data
+        }
+        set
+        {
+            image_data = newValue.tiffRepresentation ?? Data() //Convert NSImage to image data
+        }
+    }
+    
+    ///Returns info for object card view (with NSImage).
+    public var card_info: (title: String, subtitle: String, color: Color, image: NSImage)
+    {
+        return("Title", "Subtitle", Color.clear, NSImage())
+    }
+    #else
+    ///Workspace object preview image.
+    public var image: UIImage
+    {
+        get
+        {
+            return UIImage(data: image_data) ?? UIImage() //Retrun UIImage from image data
+        }
+        set
+        {
+            image_data = newValue.pngData() ?? Data() //Convert UIImage to image data
+        }
+    }
+    
+    ///Returns info for object card view (with UIImage).
+    public var card_info: (title: String, subtitle: String, color: Color, image: UIImage)
+    {
+        return("Title", "Subtitle", Color.clear, UIImage())
+    }
+    #endif
+}
