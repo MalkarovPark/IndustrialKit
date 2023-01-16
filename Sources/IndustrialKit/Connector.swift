af79c01ca343148e27b8fb2b3315d851c8c58d21
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2023-01-16 17:41:34 +0300

    Discard previous change...

--- Sources/IndustrialKit/Connector.swift
+++ Sources/IndustrialKit/Connector.swift
@@ -29,7 +29,7 @@ open class WorkspaceObjectConnector
     public var connection_updating: Bool = false
     
     ///An array of connection parameters.
-    open var parameters = [ConnectionParameter]()
+    public var parameters = [ConnectionParameter]()
     
     private var connection_task = Task {}
     private var disconnection_task = Task {}
