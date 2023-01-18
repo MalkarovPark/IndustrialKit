b56252fc4d37f28f2689e46618cf0883228e9f18
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2023-01-18 17:09:41 +0300

    Connection updating variable with published wrapper.

--- Sources/IndustrialKit/Connector.swift
+++ Sources/IndustrialKit/Connector.swift
@@ -43,7 +43,7 @@ open class WorkspaceObjectConnector: ObservableObject
     @Published public var connected: Bool = false
     
     ///A connection in updating process state.
-    public var connection_updating: Bool = false
+    @Published public var connection_updating: Bool = false
     
     ///An array of connection parameters.
     public var parameters = [ConnectionParameter]()
