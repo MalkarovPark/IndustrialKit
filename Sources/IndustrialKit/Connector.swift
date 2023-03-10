a38050b7accdf17f36130aa64499cff5b171fe72
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2023-03-10 12:41:05 +0300

    Update model flag fixed.

--- Sources/IndustrialKit/Connector.swift
+++ Sources/IndustrialKit/Connector.swift
@@ -168,7 +168,7 @@ open class WorkspaceObjectConnector: ObservableObject
     }
     
     ///A flag of update model avalibility.
-    //@Published public var update_model = false
+    @Published public var update_model = false
     
     //MARK: UI functions
     ///A failure result of connection.
