1ac5731260fb2537b46f96316b6e028a85e25f1a
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2024-04-17 23:37:38 +0300

    Name changes for the AddNewView.

--- Sources/IndustrialKit/InterfaceElements/AddNewView.swift
+++ Sources/IndustrialKit/InterfaceElements/AddNewView.swift
@@ -11,21 +11,21 @@ public struct AddNewView: View
 {
     @Binding var is_presented: Bool
     
-    @State private var new_file_name = ""
+    @State private var new_item_name = ""
     
-    private var add_file: (String) -> Void
+    private var on_item_add: (String) -> Void
     private var names: [String]?
     
-    public init(is_presented: Binding<Bool>, add_file: @escaping (String) -> Void)
+    public init(is_presented: Binding<Bool>, on_item_add: @escaping (String) -> Void)
     {
         self._is_presented = is_presented
-        self.add_file = add_file
+        self.on_item_add = on_item_add
     }
     
-    public init(is_presented: Binding<Bool>, names: [String], add_file: @escaping (String) -> Void)
+    public init(is_presented: Binding<Bool>, names: [String], on_item_add: @escaping (String) -> Void)
     {
         self._is_presented = is_presented
-        self.add_file = add_file
+        self.on_item_add = on_item_add
         self.names = names
     }
     
@@ -33,7 +33,7 @@ public struct AddNewView: View
     {
         HStack(spacing: 0)
         {
-            TextField("Name", text: $new_file_name)
+            TextField("Name", text: $new_item_name)
                 .padding(.trailing)
                 .frame(minWidth: 96)
             
@@ -48,22 +48,22 @@ public struct AddNewView: View
     
     private func name_perform()
     {
-        if new_file_name == ""
+        if new_item_name == ""
         {
-            new_file_name = "Name"
+            new_item_name = "Name"
         }
         
         if names != nil
         {
-            new_file_name = mismatched_name(name: new_file_name, names: names!)
+            new_item_name = mismatched_name(name: new_item_name, names: names!)
         }
         
-        add_file(new_file_name)
+        on_item_add(new_item_name)
         is_presented = false
     }
 }
 
 #Preview
 {
-    AddNewView(is_presented: .constant(true), add_file: { _ in })
+    AddNewView(is_presented: .constant(true), on_item_add: { _ in })
 }
