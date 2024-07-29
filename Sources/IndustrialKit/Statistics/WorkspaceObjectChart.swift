f96118e2c75eb83fcf748c2b6b0e51a5a22ffdc9
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2024-07-29 17:31:36 +0300

    Discard test...

--- Sources/IndustrialKit/Statistics/WorkspaceObjectChart.swift
+++ Sources/IndustrialKit/Statistics/WorkspaceObjectChart.swift
@@ -11,7 +11,7 @@ import Foundation
 /**
  A storage for chart info.
  */
-public class WorkspaceObjectChart: Identifiable, Codable, Hashable, ObservableObject
+public class WorkspaceObjectChart: Identifiable, Codable, Hashable
 {
     public static func == (lhs: WorkspaceObjectChart, rhs: WorkspaceObjectChart) -> Bool
     {
@@ -43,7 +43,7 @@ public class WorkspaceObjectChart: Identifiable, Codable, Hashable, ObservableOb
         return first_domain == "" ? false : true
     }
     
-    @Published public var data = [ChartDataItem]()
+    public var data = [ChartDataItem]()
     
     public init()
     {
@@ -62,72 +62,21 @@ public class WorkspaceObjectChart: Identifiable, Codable, Hashable, ObservableOb
         self.name = name
         self.style = style
     }
-    
-    //MARK: Codable handling
-    enum CodingKeys: String, CodingKey
-    {
-        case name
-        case style
-        case data
-    }
-    
-    public required init(from decoder: any Decoder) throws
-    {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.name = try container.decode(String.self, forKey: .name)
-        self.style = try container.decode(ChartStyle.self, forKey: .style)
-        self.data = try container.decode([ChartDataItem].self, forKey: .data)
-    }
-    
-    public func encode(to encoder: any Encoder) throws
-    {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(name, forKey: .name)
-        try container.encode(style, forKey: .style)
-        try container.encode(data, forKey: .data)
-    }
 }
 
-public class ChartDataItem: Identifiable, Codable
+public struct ChartDataItem: Identifiable, Codable
 {
-    @Published public var name: String
+    public var id = UUID()
+    public var name: String
     
-    @Published public var domain: [String: Float]
-    @Published public var codomain: Float
+    public var domain: [String: Float]
+    public var codomain: Float
     
     public init(name: String, domain: [String: Float], codomain: Float)
     {
         self.name = name
         self.domain = domain
         self.codomain = codomain
-    }
-    
-    //MARK: Codable handling
-    enum CodingKeys: String, CodingKey
-    {
-        case name
-        case domain
-        case codomain
-    }
-    
-    public required init(from decoder: any Decoder) throws
-    {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.name = try container.decode(String.self, forKey: .name)
-        self.domain = try container.decode([String: Float].self, forKey: .domain)
-        self.codomain = try container.decode(Float.self, forKey: .codomain)
-    }
-    
-    public func encode(to encoder: any Encoder) throws
-    {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(name, forKey: .name)
-        try container.encode(domain, forKey: .domain)
-        try container.encode(codomain, forKey: .codomain)
     }
 }
 
