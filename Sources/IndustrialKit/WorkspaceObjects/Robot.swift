597e699b3265c75a72bafa7a36752bc242cb9ae0
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2024-11-01 23:01:38 +0300

    Test...

--- Sources/IndustrialKit/WorkspaceObjects/Robot.swift
+++ Sources/IndustrialKit/WorkspaceObjects/Robot.swift
@@ -858,7 +858,7 @@ public class Robot: WorkspaceObject
         //MARK: Place workcell box
         #if os(macOS)
         space_node?.position.x = CGFloat(origin_location[1])
-        space_node?.position.y = CGFloat(origin_location[2]) + (tool_node?.position.y ?? 0)
+        space_node?.position.y = CGFloat(origin_location[2]) + (tool_node?.worldPosition.y ?? 0)
         print(tool_node?.position.y ?? 0)
         print(vertical_length ?? 0)
         //space_node?.position.y = CGFloat(origin_location[2] + (vertical_length ?? 0)) //Add vertical base length
