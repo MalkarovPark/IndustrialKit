179c19181a39c57590b89c648c6f51ea9a56e490
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2024-11-16 01:07:41 +0300

    Fix...

--- Sources/IndustrialKit/WorkspaceObjects/Robot.swift
+++ Sources/IndustrialKit/WorkspaceObjects/Robot.swift
@@ -99,7 +99,6 @@ public class Robot: WorkspaceObject
         node = module.node.clone()
         
         model_controller = module.model_controller
-        model_controller.nodes_names = module.nodes_names
         connector = module.connector
         
         apply_statistics_flags()
