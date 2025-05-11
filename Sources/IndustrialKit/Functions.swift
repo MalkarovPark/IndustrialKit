587ee3e965d0a6a06fc961ee330f39b7a3a1b830
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2025-05-11 14:01:36 +0300

    Cleanups.

--- Sources/IndustrialKit/Functions.swift
+++ Sources/IndustrialKit/Functions.swift
@@ -390,12 +390,10 @@ public func perform_terminal_app(at url: URL, with arguments: [String], timeout:
         { output in
             collected_output += output
         }
-        //return collected_output
     }
     catch
     {
-        //return collected_output
-        //return nil
+        
     }
     
     return collected_output
