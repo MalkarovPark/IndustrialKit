699ce0e86d0e981f4d801d9081507d6d57665fc9
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2025-05-19 21:39:28 +0300

    Fix...

--- Sources/IndustrialKit/Statistics/StateItem.swift
+++ Sources/IndustrialKit/Statistics/StateItem.swift
@@ -22,12 +22,43 @@ public struct StateItem: Identifiable, Codable
     
     public var is_expanded = false
     
-    // MARK: Init function
     public init(name: String, value: String? = nil, image: String? = nil, children: [StateItem]? = nil)
     {
         self.name = name
         self.value = value
         self.image = image
         self.children = children
+    }
+    
+    // MARK: - Work with file system
+    enum CodingKeys: String, CodingKey
+    {
+        case id
+        case name
+        case value
+        case image
+        case children
+    }
+    
+    public init(from decoder: any Decoder) throws
+    {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id       = try container.decode(UUID.self, forKey: .id)
+        self.name     = try container.decode(String.self, forKey: .name)
+        self.value    = try container.decodeIfPresent(String.self, forKey: .value)
+        self.image    = try container.decodeIfPresent(String.self, forKey: .image)
+        self.children = try container.decodeIfPresent([StateItem].self, forKey: .children)
+    }
+    
+    public func encode(to encoder: any Encoder) throws
+    {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(value, forKey: .value)
+        try container.encode(image, forKey: .image)
+        try container.encode(children, forKey: .children)
     }
 }
