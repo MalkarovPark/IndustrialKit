664ff35a989a3d7015ecc25f296e0b8ddbcbad0d
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2025-05-30 13:30:20 +0300

    Discard test...

--- Sources/IndustrialKit/WorkspaceObjects/Robot.swift
+++ Sources/IndustrialKit/WorkspaceObjects/Robot.swift
@@ -1380,12 +1380,7 @@ public class Robot: WorkspaceObject
         try super.init(from: decoder)
         
         self.connector.import_connection_parameters_values(try container.decodeIfPresent([String].self, forKey: .connection_parameters))
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2)
-        {
-            self.reset_pointer_to_default()
-        }
-        //self.reset_pointer_to_default()
+        self.reset_pointer_to_default()
     }
     
     public override func encode(to encoder: any Encoder) throws
