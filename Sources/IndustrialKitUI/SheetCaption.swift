1d7185491f86bbd167e78d51d607f1a83e274823
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2025-07-24 19:18:53 +0300

    Fix...

--- Sources/IndustrialKitUI/SheetCaption.swift
+++ Sources/IndustrialKitUI/SheetCaption.swift
@@ -32,7 +32,7 @@ public struct SheetCaption: ViewModifier
                     #if os(macOS)
                     Spacer(minLength: 52)
                     #elseif os(iOS)
-                    Spacer(minLength: 56)
+                    Spacer(minLength: 60)
                     #else
                     Spacer(minLength: 56)
                     #endif
