6a0bec1dc997bf2ec2e2c1d7da9aa64a6c2a07ac
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2025-12-20 23:47:08 +0300

    Discard...

--- Sources/IndustrialKit/Workspace.swift
+++ Sources/IndustrialKit/Workspace.swift
@@ -461,12 +461,8 @@ public class Workspace: ObservableObject, @unchecked Sendable
     /// Selects and performs program element by workspace.
     private func perform_next_element()
     {
-        DispatchQueue.main.async {
-            self.selected_program_element.performing_state = .processing //BOOM
-            self.perform(self.selected_program_element, completion: self.select_new_element)
-        }
-        //selected_program_element.performing_state = .processing //BOOM
-        //perform(selected_program_element, completion: select_new_element)
+        selected_program_element.performing_state = .processing //BOOM
+        perform(selected_program_element, completion: select_new_element)
     }
     
     private func perform_constant_objects_update()
