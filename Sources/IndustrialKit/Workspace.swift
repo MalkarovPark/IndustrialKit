56da3c9512df66d63ef1323dc55fe48e6a9b5cbc
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2026-02-13 23:08:48 +0300

    Discard...

--- Sources/IndustrialKit/Workspace.swift
+++ Sources/IndustrialKit/Workspace.swift
@@ -1637,7 +1637,8 @@ public class Workspace: ObservableObject, @unchecked Sendable
             {
                 let batchEnd = min(batchStart + 20, indices.count)
                 
-                await MainActor.run {
+                await MainActor.run
+                {
                     for i in batchStart..<batchEnd
                     {
                         let idx = indices[i]
@@ -1646,7 +1647,7 @@ public class Workspace: ObservableObject, @unchecked Sendable
                     }
                 }
                 
-                try? await Task.sleep(nanoseconds: 1_000_000_0) // 1ms пауза между батчами
+                try? await Task.sleep(nanoseconds: 1_000_000_0)
             }
         }
     }
@@ -2586,50 +2587,13 @@ public class Workspace: ObservableObject, @unchecked Sendable
             {
                 let end_point_entity = robot_by_name(attached_to).end_point_entity
                 
-                Task
-                {
-                    await wait_loading(entity: end_point_entity)
-                    end_point_entity.addChild(tool.entity)
-                }
-            }
-            else
-            {
-                Task
-                {
-                    await wait_loading(entity: workspace_entity)
-                    workspace_entity.addChild(tool.entity)
-                }
-            }
-        }
-        
-        func wait_loading(entity: Entity) async
-        {
-            while !is_loaded(entity)
-            {
-                try? await Task.sleep(nanoseconds: 50_000_000)
-            }
-        }
-        
-        func is_loaded(_ entity: Entity) -> Bool
-        {
-            return !entity.children.isEmpty || entity.components[ModelComponent.self] != nil
-        }
-        
-        /*if !(tools.count > 0) { return }
-        
-        for tool in tools
-        {
-            if let attached_to = tool.attached_to
-            {
-                let end_point_entity = robot_by_name(attached_to).end_point_entity
-                
                 end_point_entity.addChild(tool.entity)
             }
             else
             {
                 workspace_entity.addChild(tool.entity)
             }
-        }*/
+        }
         
         /*for tool in tools
         {
