5c20518aedf3f386e5d153c2482857e656c10460
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2026-03-19 21:45:20 +0300

    Test...

--- Sources/IndustrialKit/Functions.swift
+++ Sources/IndustrialKit/Functions.swift
@@ -368,6 +368,7 @@ public func perform_terminal_app_sync(at url: URL, with arguments: [String])
 {
     do
     {
+        print("'\(url.path)' \(arguments.joined(separator: " "))")
         try perform_terminal_command("'\(url.path)' \(arguments.joined(separator: " "))", timeout: 0.1)
     }
     catch
