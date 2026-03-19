8de926724723ccef88827be432e343ea01845ecf
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2026-03-20 00:20:42 +0300

    Fix...

--- Sources/IndustrialKit/Connectors/ToolConnector.swift
+++ Sources/IndustrialKit/Connectors/ToolConnector.swift
@@ -272,7 +272,7 @@ public class ExternalToolConnector: ToolConnector, ExternalConnector, @unchecked
     
     public var socket_name: String
     {
-        return "/tmp/\(module_name)_\(id)_tool_connector_socket"
+        return "/tmp/\(module_name)_\(Int(bitPattern: id))_tool_connector_socket" //self.id.hashValue
     }
     
     // MARK: Parameters import
