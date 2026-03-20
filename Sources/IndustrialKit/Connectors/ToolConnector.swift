2a292a97c99ac70350e912ef16368491ab4b6ce9
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2026-03-20 14:10:37 +0300

    Test...

--- Sources/IndustrialKit/Connectors/ToolConnector.swift
+++ Sources/IndustrialKit/Connectors/ToolConnector.swift
@@ -161,8 +161,8 @@ public class ExternalToolConnector: ToolConnector, ExternalConnector, @unchecked
         copy.module_name = module_name
         copy.package_url = package_url
         
-        copy.external_parameters = external_parameters
-        copy.parameters = parameters
+        copy.external_parameters = external_parameters //default_parameters
+        copy.parameters = external_parameters
         
         return copy
     }
