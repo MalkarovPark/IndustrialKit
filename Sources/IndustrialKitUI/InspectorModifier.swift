e685a3cd6da409e062f9807320740e6597712de6
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2026-07-28 17:21:27 +0300

    Test fix...

--- Sources/IndustrialKitUI/InspectorModifier.swift
+++ Sources/IndustrialKitUI/InspectorModifier.swift
@@ -28,6 +28,7 @@ public struct InspectorModifier<InspectorContent: View>: ViewModifier
                     .background(.thickMaterial)
                     .fixedSize(horizontal: true, vertical: false)
                     .transition(.move(edge: .trailing))
+                    .backgroundExtensionEffect()
             }
         }
         .animation(.easeInOut, value: is_presented)
